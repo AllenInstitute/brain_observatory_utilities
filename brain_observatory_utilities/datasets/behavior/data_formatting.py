d068dacbbc034c509dd1e1d848c45b4e119792ae
matchings <marinag@alleninstitute.org>
2022-08-23 11:39:55 -0700

    add stim presentation annotation functions to behavior module

--- brain_observatory_utilities/datasets/behavior/data_formatting.py
+++ brain_observatory_utilities/datasets/behavior/data_formatting.py
@@ -55,6 +55,58 @@ def add_mean_running_speed_to_stimulus_presentations(stimulus_presentations,
     return stimulus_presentations
 
 
+def add_mean_pupil_to_stimulus_presentations(stimulus_presentations, eye_tracking, column_to_use='pupil_area', time_window=[0, 0.75]):
+    '''
+    Append a column to stimulus_presentations which contains
+    the mean pupil area, diameter, or radius in a range relative to
+    the stimulus start time.
+
+    Args:
+        stimulus_presentations (pd.DataFrame): dataframe of stimulus presentations.  # noqa E501
+            Must contain: 'start_time'
+        eye_tracking (pd.DataFrame): dataframe of eye tracking data.
+            Must contain: timestamps', 'pupil_area', 'pupil_width', 'likely_blinks'
+        time_window (list with 2 elements): start and end of the range
+            relative to the start of each stimulus to average the pupil area.
+        column_to_use: column in eyetracking table to use to get mean, options: 'pupil_area', 'pupil_width', 'pupil_radius', 'pupil_diameter'
+                        if 'pupil_diameter' or 'pupil_radius' are provided, they will be calculated from 'pupil_area'
+                        if 'pupil_width' is provided, the column 'pupil_width' will be directly used from eye_tracking table
+    Returns:
+        stimulus_presentations table with new column "mean_pupil_"+column_to_use with the
+        mean pupil value within the specified window following each stimulus presentation.
+
+    Example:
+        # get visual behavior cache
+        from allensdk.brain_observatory.behavior.behavior_project_cache import VisualBehaviorOphysProjectCache as bpc  # noqa E501
+        cache_dir = r'\\allen\programs\braintv\workgroups\nc-ophys\visual_behavior\platform_paper_cache'
+        cache = bpc.from_s3_cache(cache_dir=cache_dir)
+
+        # load data for one experiment
+        ophys_experiment = cache.get_behavior_ophys_experiment(experiment_id)
+
+        # get necessary tables
+        stimulus_presentations = ophys_experiment.stimulus_presentations.copy()
+        eye_tracking = ophys_experiment.eye_tracking.copy()
+
+        # add pupil area to stim presentations
+        stimulus_presentations = add_mean_pupil_to_stimulus_presentations(stimulus_presentations, eye_tracking, column_to_use='pupil_area')  # noqa E501
+    '''
+
+    eye_tracking = get_pupil_data(eye_tracking, interpolate_likely_blinks=True, normalize_to_gray_screen=True, zscore=False,
+                   interpolate_to_ophys=False, ophys_timestamps=None, stimulus_presentations=stimulus_presentations)
+
+    eye_tracking_timeseries = eye_tracking[column_to_use].values
+    mean_pupil_around_stimulus = stimulus_presentations.apply(
+        lambda row: get_trace_average(
+            eye_tracking_timeseries,
+            eye_tracking['timestamps'].values,
+            row["start_time"] + time_window[0],
+            row["start_time"] + time_window[1],
+        ), axis=1,)
+    stimulus_presentations["mean_"+column_to_use] = mean_pupil_around_stimulus
+    return stimulus_presentations
+
+
 def add_rewards_to_stimulus_presentations(stimulus_presentations,
                                           rewards,
                                           time_window=[0,3]):
@@ -188,5 +240,84 @@ def add_reward_rate_to_stimulus_presentations(stimulus_presentations,
         reward_rate_by_frame.append(reward_rate_by_frame[-1])
 
     stimulus_presentations['reward_rate_trials'] = reward_rate_by_frame
+    return stimulus_presentations
+
+
+def add_epochs_to_stimulus_presentations(stimulus_presentations, time_column='start_time', epoch_duration_mins=10):
+    """
+    Add column called 'epoch' with values as an index for the epoch within a session, for a given epoch duration.
+
+    :param stimulus_presentations: dataframe with a column indicating event start times
+    :param time_column: name of column in dataframe indicating event times
+    :param epoch_duration_mins: desired epoch length in minutes
+    :return: input dataframe with epoch column added
+    """
+    start_time = stimulus_presentations[time_column].values[0]
+    stop_time = stimulus_presentations[time_column].values[-1]
+    epoch_times = np.arange(start_time, stop_time, epoch_duration_mins * 60)
+    stimulus_presentations['epoch'] = None
+    for i, time in enumerate(epoch_times):
+        if i < len(epoch_times) - 1:
+            indices = stimulus_presentations[(stimulus_presentations[time_column] >= epoch_times[i]) &
+                                             (stimulus_presentations[time_column] < epoch_times[i + 1])].index.values
+        else:
+            indices = stimulus_presentations[(stimulus_presentations[time_column] >= epoch_times[i])].index.values
+        stimulus_presentations.at[indices, 'epoch'] = i
+    return stimulus_presentations
+
+
+def add_trials_id_to_stimulus_presentations(stimulus_presentations, trials):
+    """
+    Add trials_id to stimulus presentations by finding the closest change time to each stimulus start time
+    If there is no corresponding change time, the trials_id is NaN
+    :param: stimulus_presentations: stimulus_presentations attribute of BehaviorOphysExperiment object, must have 'start_time'
+    :param trials: trials attribute of BehaviorOphysExperiment object, must have 'change_time'
+    """
+    # for each stimulus_presentation, find the trials_id that is closest to the start time
+    # add to a new column called 'trials_id'
+    for idx, stimulus_presentation in stimulus_presentations.iterrows():
+        start_time = stimulus_presentation['start_time']
+        query_string = 'change_time > @start_time - 1 and change_time < @start_time + 1'
+        trials_id = (np.abs(start_time - trials.query(query_string)['change_time']))
+        if len(trials_id) == 1:
+            trials_id = trials_id.idxmin()
+        else:
+            trials_id = np.nan
+        stimulus_presentations.loc[idx, 'trials_id'] = trials_id
+    return stimulus_presentations
+
+
+def add_trials_data_to_stimulus_presentations_table(stimulus_presentations, trials):
+    """
+    Add trials_id to stimulus presentations table then join relevant columns of trials with stimulus_presentations
+    :param: stimulus_presentations: stimulus_presentations attribute of BehaviorOphysExperiment object, must have 'start_time'
+    :param trials: trials attribute of BehaviorOphysExperiment object, must have 'change_time'
+    """
+    # add trials_id and merge to get trial type information
+    stimulus_presentations = add_trials_id_to_stimulus_presentations(stimulus_presentations, trials)
+    # only keep certain columns
+    trials = trials[['change_time', 'go', 'catch', 'aborted', 'auto_rewarded',
+                    'hit', 'miss', 'false_alarm', 'correct_reject',
+                    'response_time', 'response_latency', 'reward_time', 'reward_volume', ]]
+    # merge trials columns into stimulus_presentations
+    stimulus_presentations = stimulus_presentations.reset_index().merge(trials, on='trials_id', how='left')
+    stimulus_presentations = stimulus_presentations.set_index('stimulus_presentations_id')
+    return stimulus_presentations
+
+
+
+def add_time_from_last_change_to_stimulus_presentations(stimulus_presentations):
+    '''
+    Adds a column to stimulus_presentations called 'time_from_last_change', which is the time, in seconds since the last image change
+
+    ARGS: SDK session object
+    MODIFIES: session.stimulus_presentations
+    RETURNS: stimulus_presentations
+    '''
+    stimulus_times = stimulus_presentations["start_time"].values
+    change_times = stimulus_presentations.query('is_change')['start_time'].values
+    time_from_last_change = general_utilities.time_from_last(stimulus_times, change_times)
+    stimulus_presentations["time_from_last_change"] = time_from_last_change
+
     return stimulus_presentations
 
