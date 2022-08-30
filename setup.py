6f58d86725665a30499854ffe3e767085f241685
matchings <marinag@alleninstitute.org>
2022-08-29 17:56:50 -0700

    increase version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="brain_observatory_utilities",
-    version="0.1.9",
+    version="0.1.10",
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     description="Utilities for analyzing, manipulating and visualizing data for Brain Observatory projects",
