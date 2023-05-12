5e6c5e0dd51add37ead4b985fd3080bf94d9c735
iryna.yavorska <irynayavorska@gmail.com>
2023-05-12 16:08:15 -0700

    plot_response_matrix

--- brain_observatory_utilities/plotting/behavior_plots.py
+++ brain_observatory_utilities/plotting/behavior_plots.py
@@ -151,6 +151,65 @@ def plot_pupil_area(dataObject, ax=None):
     return ax
 
 
+
+def plot_response_matrix(stimulus_presentations, ax=None, vmin=0, vmax=1, cmap='viridis', cbar_ax=None):
+    '''
+    makes a plot of the response matrix given a table of stimuli
+
+    Parameters:
+    -----------
+    stimulus_presentations : pandas.dataframe
+        From experiment.stimulus_presentations, after annotating as follows:
+            annotate_stimuli(experiment, inplace = True)
+    ax : matplotlib axis
+        axis on which to plot.
+        If not passed, will create a figure and axis and return both
+    vmin : float
+        minimum for plot, default = 0
+    vmax : float
+        maximum for plot, default = 1
+    cmap : string
+        cmap for plot, default = 'viridis'
+    cbar_ax : matplotlib axis
+        axis on which to plot colorbar
+        colorbar will not be added if not passed
+
+    Returns:
+    --------
+    fig, ax if ax is not passed
+    None if ax is passed
+    '''
+
+    return_fig_ax = False
+    if ax is None:
+        return_fig_ax = True
+        fig, ax = plt.subplots()
+
+    response_matrix = calculate_response_matrix(stimulus_presentations)
+
+    im = ax.imshow(
+        response_matrix,
+        cmap=cmap,
+        aspect='equal',
+        vmin=vmin,
+        vmax=vmax,
+    )
+    ax.set_xticks(range(len(response_matrix)))
+    ax.set_xticklabels(response_matrix.columns)
+    ax.set_yticks(range(len(response_matrix)))
+    ax.set_yticklabels(response_matrix.index)
+    ax.set_xlabel('image name')
+    ax.set_ylabel('previous image name')
+
+    if cbar_ax is not None:
+        plt.colorbar(im, cax=cbar_ax, label='response probability')
+
+    if return_fig_ax:
+        return fig, ax
+
+
+
+
 if __name__ == "__main__":
     # make_plots(experiment_dataset) - test
     pass
