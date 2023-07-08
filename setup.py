e70b0c2161c1fd91da36924093961c4f2b8868d0
matchings <marinag@alleninstitute.org>
2023-07-07 20:33:14 -0700

    update pypi download url

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="brain_observatory_utilities",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     description="Utilities for analyzing, manipulating and visualizing data for Brain Observatory projects",
-    dowload_url='https://github.com/AllenInstitute/brain_observatory_utilities/archive/refs/tags/v0.2.0.tar.gz',
+    dowload_url='https://github.com/AllenInstitute/brain_observatory_utilities/archive/refs/tags/v0.2.1.tar.gz',
     url="https://github.com/AllenInstitute/mindscope_utilities",
     author="Allen Institute",
     author_email="corbettb@alleninstitute.org, michaelbu@alleninstitute.org, marinag@alleninstitute.org, clark.roll@alleninstitute.org",
