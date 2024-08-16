490b13afd55b6ff790f822c380132ab712c95b4c
Marina <marinag@alleninstitute.org>
2024-08-15 20:35:44 -0700

    upgrade version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="brain_observatory_utilities",
-    version="0.3.1",
+    version="0.4.0",
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     description="Utilities for analyzing, manipulating and visualizing data for Brain Observatory projects",
-    dowload_url='https://github.com/AllenInstitute/brain_observatory_utilities/archive/refs/tags/v0.2.1.tar.gz',
+    dowload_url='https://github.com/AllenInstitute/brain_observatory_utilities/archive/refs/tags/v0.4.0.tar.gz',
     url="https://github.com/AllenInstitute/mindscope_utilities",
     author="Allen Institute",
     author_email="corbettb@alleninstitute.org, michaelbu@alleninstitute.org, marinag@alleninstitute.org, clark.roll@alleninstitute.org",
